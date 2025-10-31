d15f6ad40afd6bb91a7d259e8ce14eee7b9601f7
jeffhg <jefferyhg7889@gmail.com>
2025-10-30 21:41:03 -0400

    Initial commit

---
+++ src/test/java/io/github/jeff_hg/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeff_hg.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
